c53355676df77fe083ea880b94b7779ef01f67d7
Jean Cremonezzi <jeancremonezzidev@gmail.com>
2023-10-17 14:44:46 -0300

    chore: Changed cors

--- src/Config/Server.ts
+++ src/Config/Server.ts
@@ -13,7 +13,7 @@ export const appConfig = async (app: Express) => {
     await connect(`${process.env.DB_URI}`)
     
     app.use(cors({
-        origin : '*',
+        origin : ['http://localhost:5173'],
         credentials: true
     }))
 
