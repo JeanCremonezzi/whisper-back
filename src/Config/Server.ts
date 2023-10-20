f7f8129668d431d0106b4864cf1d35a2608dff14
Jean Cremonezzi <jeancremonezzidev@gmail.com>
2023-10-20 13:52:35 -0300

    refact: Socket config in separate file

--- src/Config/Server.ts
+++ src/Config/Server.ts
@@ -7,6 +7,7 @@ import cookieParser from 'cookie-parser';
 import { Server } from "socket.io";
 import { createServer } from 'http';
 import { User } from '../Database/Models/User/User';
+import { SocketConfig } from './SocketConfig';
 
 require('dotenv').config()
 
@@ -22,44 +23,7 @@ export const appConfig = async (app: Express) => {
     app.use(cookieParser())
     app.use(routes)
 
-    const httpServer = createServer(app);
-    const io = new Server(httpServer, {
-        cors: {
-            origin: '*',
-        },
-    });
-
-    const rooms = new Map<string, string>()
-
-    io.use(async (socket, next) => {
-        if (!socket.handshake.auth.user || !await User.findOne({email: JSON.parse(socket.handshake.auth.user).email})) {
-            next(new Error("Invalid User"));
-            return
-        }
-        
-        socket.data.user = JSON.parse(socket.handshake.auth.user)
-        next()
-    });
-
-    io.on("connection", (socket) => {        
-        rooms.set(socket.data.user.email, socket.id)
-
-        socket.on("message", ({message, to}) => {
-            if (rooms.has(to)) {
-                const toRoom = rooms.get(to)!
-                socket.to(toRoom).emit("message", {
-                    message,
-                    from: socket.data.user.email
-                })
-            }
-
-            // TODO - PERSISTIR MENSAGEM
-        })
-        
-        socket.on('disconnect', () => {
-            rooms.delete(socket.handshake.auth.email)            
-        });
-    });
+    const httpServer = SocketConfig(app)
 
     httpServer.listen(process.env.PORT, () => {
         console.log(`\nApp running on port ${process.env.PORT}\n`)
---
+++ src/Config/SocketConfig.ts
@@ -0,0 +1,50 @@
+import { Express } from 'express'
+import { createServer } from "http";
+import { Server } from "socket.io";
+import { User } from "../Database/Models/User/User";
+
+export const SocketConfig = (app: Express) => {
+    const rooms = new Map<string, string>()
+
+    const httpServer = createServer(app);
+
+    const io = new Server(httpServer, {
+        cors: {
+            origin: '*',
+        },
+    });
+
+    io.use(async (socket, next) => {
+        if (!socket.handshake.auth.user || !await User.findOne({ email: socket.handshake.auth.user.email })) {
+            next(new Error("Invalid User"));
+            return
+        }
+        
+        socket.data.user = socket.handshake.auth.user
+        next()
+    });
+
+    io.on("connection", (socket) => {
+        console.log(rooms);
+              
+        rooms.set(socket.data.user.email, socket.id)
+
+        socket.on("message", ({message, to}) => {
+            if (rooms.has(to)) {
+                const toRoom = rooms.get(to)!
+                socket.to(toRoom).emit("message", {
+                    message,
+                    from: socket.data.user.email
+                })
+            }
+
+            // TODO - PERSISTIR MENSAGEM
+        })
+        
+        socket.on('disconnect', () => {
+            rooms.delete(socket.handshake.auth.email)
+        });
+    });
+
+    return httpServer
+}
